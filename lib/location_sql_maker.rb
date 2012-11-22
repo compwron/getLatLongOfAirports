d65dc898e659c89b2947ecd2b99eeddde3514285
compwron <wordymusic@gmail.com>
2012-11-22 07:25:36 -0600

    WIP, end to end testing, plane landing

--- lib/location_sql_maker.rb
+++ lib/location_sql_maker.rb
@@ -1,30 +1,35 @@
 class LocationSqlMaker 
 
-def initialize stations
-  @airport_codes = airport_codes
-end
+   @@output_location = 'output/curl'
 
-def make_sql 
-airport_codes.map { |airport_code| 
-  if File.exist?("#{airport_code.html}") then 
-    make_sql_from_html(airport_code)
-  else
-    get_html_for_airport_code(airport_code)
+   attr_accessor :airport_codes
+
+  def initialize stations
+    @airport_codes = airport_codes
   end
-}
 
-def get_html_for_airport_code airport_code
-  `curl --silent http://airnav.com/airport/#{airport_code}` > "#{airport_code}.html"
-end
+  def make_sql 
+  airport_codes.map { |airport_code| 
+    if File.exist?("@@output_location/#{airport_code.html}") then 
+      make_sql_from_html(airport_code)
+    else
+      get_html_for_airport_code(airport_code)
+    end
+  }
+  end 
 
-def make_sql_from_html airport_code
-  f = File.open("#{airport_code}.html")
-  contents = f.read(f)
-  matcher = contents.match /.*latitude=(.*)&longitude=(.*)&name.*/
-  if (!matcher.nil? && matcher[1] && matcher[2]) then 
-    latitude = matcher[1]
-    longitude = matcher[2]
+  def get_html_for_airport_code airport_code
+    `curl --silent http://airnav.com/airport/#{airport_code}` > "@@output_location/#{airport_code}.html"
   end
-  `echo "update station set latitude=(#{latitude}), longitude=(#{longitude}) where airport_code = '#{airport_code}';" >> sql.txt`
-end
+
+  def make_sql_from_html airport_code
+    f = File.open("#{@@output_location}/#{airport_code}.html")
+    contents = f.read
+    matcher = contents.match /.*latitude=(.*)&longitude=(.*)&name.*/
+    if (!matcher.nil? && matcher[1] && matcher[2]) then 
+      latitude = matcher[1]
+      longitude = matcher[2]
+    end
+    "update station set latitude=(#{latitude}), longitude=(#{longitude}) where airport_code = '#{airport_code}';"
+  end
 end
--- spec/get_station_html_spec.rb
+++ spec/get_station_html_spec.rb
@@ -1,9 +1,18 @@
-require_relative "../get_station_html.rb"
+require_relative "../lib/location_sql_maker.rb"
 
-describe GetStationHtml do
+describe LocationSqlMaker do
 
-  describe "#" do
-    it "should " do
+  describe "#thing" do
+    subject = LocationSqlMaker.new(['BAF'])
+
+    it "should make sql out of file" do
+      
+      expected_sql = "update station set latitude=(42.157944), longitude=(-72.715861) where airport_code = 'BAF';"
+      subject.make_sql_from_html('BAF').should == expected_sql
+    end
+
+    it "should get html file from website" do
+      subject.get_fi
     end
   end
 end
